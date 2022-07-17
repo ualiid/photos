d1582df7d8f957473f4a439d1aa0b1ce250ede50
OUALID EL MORABIT <77552716+ualiid@users.noreply.github.com>
2022-07-18 01:34:57 +0200

    Update datta.js

--- src/datta.js
+++ src/datta.js
@@ -9,24 +9,24 @@ const bird_images = ['100_4037.JPG',
 
 const data=[
     {
-        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[0]}`,
-        img_name:`${bird_images[0]}`
+        img: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bir_mages[0]}`,
+        image_name:`${bir_mages[0]}`
     },
     {
-        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[1]}`,
-        img_name: `${bird_images[1]}`
+        img: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[1]}`,
+        image_name: `${bird_images[1]}`
     },
     {
-        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[2]}`,
-        img_name: `${bird_images[2]}`
+        img: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[2]}`,
+        image_name: `${bird_images[2]}`
     },
     {
-        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[3]}`,
-        img_name: `${bird_images[3]}`
+        img: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[3]}`,
+        image_name: `${bird_images[3]}`
     },
     {
-        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[4]}`,
-        img_name: `${bird_images[4]}`
+        img: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[4]}`,
+        image_name: `${bird_images[4]}`
     },
     
 ]
