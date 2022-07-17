03beac5f9b25e0d67318c57e2bd9c38cc478683a
OUALID EL MORABIT <77552716+ualiid@users.noreply.github.com>
2022-07-18 01:31:19 +0200

    another way

---
+++ src/datta.js
@@ -0,0 +1,34 @@
+
+const bird_images = ['100_4037.JPG',
+'100_4452.JPG',
+'100_4453.JPG',
+'100_4454.JPG',
+'100_4455.JPG'];
+
+
+
+const data=[
+    {
+        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[0]}`,
+        img_name:`${bird_images[0]}`
+    },
+    {
+        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[1]}`,
+        img_name: `${bird_images[1]}`
+    },
+    {
+        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[2]}`,
+        img_name: `${bird_images[2]}`
+    },
+    {
+        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[3]}`,
+        img_name: `${bird_images[3]}`
+    },
+    {
+        image: `https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${bird_images[4]}`,
+        img_name: `${bird_images[4]}`
+    },
+    
+]
+
+export default data;   
